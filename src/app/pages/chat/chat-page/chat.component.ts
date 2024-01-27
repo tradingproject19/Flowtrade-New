1b1a155194a7ae762637fd8eb96f2889b888b707
tradingproject19 <50413570+tradingproject19@users.noreply.github.com>
2024-01-27 16:21:41 -0700

    added discord chat integration

--- src/app/pages/chat/chat-page/chat.component.ts
+++ src/app/pages/chat/chat-page/chat.component.ts
@@ -9,6 +9,7 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 export class ChatComponent {
   public safeURL: SafeResourceUrl;
   constructor(private _sanitizer: DomSanitizer) {
-  this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl('https://discord.com/widget?id=471521558989504531&theme=dark');
+  this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl('https://emerald.widgetbot.io/channels/471521558989504531/472119292427501568');
   }
 }
+// <iframe src="https://emerald.widgetbot.io/channels/299881420891881473/355719584830980096" height="600" width="800"></iframe>
