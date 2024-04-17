a84202e1937b9e97c5484918c57f8a0f8379d53a
bilalsaeed <tradingproject19@gmail.com>
2024-04-17 16:24:50 +0500

    now using new firebase account

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,20 +4,18 @@
 
 export const environment = {
   production: false,
-  defaultauth: 'firebase',
+  defaultauth: "firebase",
   firebaseConfig: {
-    apiKey: 'AIzaSyCpOX42m6eRZPD2-3x_ei8DFHcrz8KWOpw',
-    authDomain: 'tradingproject19-f513b.firebaseapp.com',
-    projectId: 'tradingproject19-f513b',
-    storageBucket: 'tradingproject19-f513b.appspot.com',
-    messagingSenderId: '422843145172',
-    measurementId: "G-XBXR27JSBH",
-    appId:'1:422843145172:web:fde9ddfa491c0952',
-    databaseURL: 'https://tradingproject19-f513b.firebaseio.com'
-  }
+    apiKey: "AIzaSyCnR2pZ-1T86KPKAUUjakBpV0h_UdRm5Co",
+    authDomain: "tradingproject24.firebaseapp.com",
+    databaseURL: "https://tradingproject24-default-rtdb.firebaseio.com",
+    projectId: "tradingproject24",
+    storageBucket: "tradingproject24.appspot.com",
+    messagingSenderId: "250728384764",
+    appId: "1:250728384764:web:76364128156e328c9f2f79",
+    measurementId: "G-6N0X8SLC3X"
+  },
 };
-
-
 
 /*
  * For easier debugging in development mode, you can import the following file
